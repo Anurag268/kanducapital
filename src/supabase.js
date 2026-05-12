2567d2846f71cc6e8b43d387746e25dc690550c5
Anurag Singh <Anuragsingh78626@gmail.com>
2026-05-12 13:28:10 +0530

    env

--- src/supabase.js
+++ src/supabase.js
@@ -10,8 +10,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
+  import.meta.env.REACT_APP_SUPABASE_URL,
+  import.meta.env.REACT_APP_SUPABASE_ANON_KEY
 );
 
 
