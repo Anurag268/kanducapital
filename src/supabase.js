73c507ebe502de248134ff8033a6a137df06b4da
Anurag Singh <Anuragsingh78626@gmail.com>
2026-05-12 13:26:52 +0530

    env fix

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,22 @@
+// import { createClient } from "@supabase/supabase-js";
+
+// const supabaseUrl = "https://hngftplrbzfchkhycivq.supabase.co";
+// const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhuZ2Z0cGxyYnpmY2hraHljaXZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3MTgyOTgsImV4cCI6MjA5MzI5NDI5OH0.Zg8fIvPop2KKO7lt6QRxH44XqZsMNwTN7NtE2VQ59Xk";
+
+// export const supabase = createClient(supabaseUrl, supabaseKey);
+
+
+
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://hngftplrbzfchkhycivq.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhuZ2Z0cGxyYnpmY2hraHljaXZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3MTgyOTgsImV4cCI6MjA5MzI5NDI5OH0.Zg8fIvPop2KKO7lt6QRxH44XqZsMNwTN7NtE2VQ59Xk";
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
+
+
+
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
