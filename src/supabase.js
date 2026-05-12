05b7cd002195c345168e662c18716c0a8ac3d7f7
Anurag Singh <89679260+Anurag268@users.noreply.github.com>
2026-05-12 13:31:16 +0530

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,9 +1,4 @@
-// import { createClient } from "@supabase/supabase-js";
 
-// const supabaseUrl = "https://hngftplrbzfchkhycivq.supabase.co";
-// const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhuZ2Z0cGxyYnpmY2hraHljaXZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3MTgyOTgsImV4cCI6MjA5MzI5NDI5OH0.Zg8fIvPop2KKO7lt6QRxH44XqZsMNwTN7NtE2VQ59Xk";
-
-// export const supabase = createClient(supabaseUrl, supabaseKey);
 
 
 
